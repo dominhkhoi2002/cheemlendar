f28490de632fe8863cb0a1b996774937316602cf
minhdn-0255 <minhdn.es76d@gmail.com>
2023-06-15 02:07:50 +0700

    ver1

--- src/app/calendar/page.tsx
+++ src/app/calendar/page.tsx
@@ -1,9 +1,25 @@
-import React from "react"
+"use client"
+import { usePathname } from "next/navigation"
+import React, { useEffect } from "react"
 
 type Props = {}
-
-const page = (props: Props) => {
-  return <div>calendar</div>
+const getWeekNumber = () => {
+  const now: any = new Date()
+  const startOfYear: any = new Date(now.getFullYear(), 0, 1)
+  const weekNumber = Math.ceil((now - startOfYear) / 604800000 + 1)
+  return weekNumber
 }
 
-export default page
+const Page = (props: Props) => {
+  const pathname = usePathname()
+
+  useEffect(() => {
+    if (pathname === "/calendar") {
+      window.location.replace(`/calendar/week/${getWeekNumber()}`)
+    }
+  }, [])
+
+  return <div></div>
+}
+
+export default Page
---
+++ src/components/Calendar/CalendarMenu.tsx
@@ -0,0 +1,64 @@
+"use client"
+import React, { useState } from "react"
+import { CalendarOutlined, HomeOutlined, SettingOutlined, TeamOutlined } from "@ant-design/icons"
+import { MenuProps, ConfigProvider, ThemeConfig } from "antd"
+import { Layout, Menu } from "antd"
+import CalendarHeader from "./CalendarHeader"
+
+const { Header, Content, Footer, Sider } = Layout
+const config: ThemeConfig = {
+  token: {
+    colorPrimary: "#ff7e04",
+    colorPrimaryBgHover: "#fff",
+  },
+}
+
+type MenuItem = Required<MenuProps>["items"][number]
+
+function getItem(label: React.ReactNode, key: React.Key, icon?: React.ReactNode): MenuItem {
+  return {
+    key,
+    icon,
+    label,
+  } as MenuItem
+}
+
+const items: MenuItem[] = [
+  getItem("Home", "1", <HomeOutlined />),
+  getItem("Calendar", "2", <CalendarOutlined />),
+  getItem("Team", "3", <TeamOutlined />),
+  getItem("Setting", "4", <SettingOutlined />),
+]
+
+const activeNavOptions = ["Day", "Week", "Month", "Year"] as const
+
+type Props = {
+  activeNav: (typeof activeNavOptions)[number]
+  content: any
+}
+const CalendarMenu = (props: Props) => {
+  const [collapsed, setCollapsed] = useState(false)
+  return (
+    <ConfigProvider theme={config}>
+      <Layout style={{ minHeight: "100vh" }}>
+        <Sider
+          collapsible
+          collapsed={collapsed}
+          onCollapse={(value) => setCollapsed(value)}
+          style={{ backgroundColor: "#fff" }}
+          theme="light"
+        >
+          <Menu defaultSelectedKeys={["2"]} mode="inline" items={items} />
+        </Sider>
+        <Layout>
+          <CalendarHeader activeNav={props.activeNav} />
+          <Content style={{ margin: "24px 16px 0" }}>
+            <div>{props.content}</div>
+          </Content>
+        </Layout>
+      </Layout>
+    </ConfigProvider>
+  )
+}
+
+export default CalendarMenu
