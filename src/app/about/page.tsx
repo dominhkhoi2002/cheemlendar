4011f7d39f57b1d10fae97c0c9e57ce29d0fd816
dominhkhoi2002 <khoi.dominh2306@gmail.com>
2023-06-18 17:57:23 +0700

    Em nham Home vs About

--- src/app/about/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import About from "@/components/Page/AboutUs/About";
-type Props = {};
-
-function page() {
-    return <About />
-  }
-    export default About;
-    
---
+++ src/app/home/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Home from "@/components/Page/Home/Home";
+type Props = {}
+
+const page = (props: Props) => {
+  return <Home></Home>
+}
+
+export default page
