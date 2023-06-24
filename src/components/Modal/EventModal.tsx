d295a5ef88e2e8bc38c9b1357c482b6d0d959346
Minh-19102 <minhdn.es76d@gmail.com>
2023-06-24 15:41:25 +0700

    update drag to create Event

--- src/components/Modal/EventModal.tsx
+++ src/components/Modal/EventModal.tsx
@@ -1,7 +1,9 @@
 import { ClockCircleOutlined, MenuOutlined } from '@ant-design/icons'
-import { Input } from 'antd'
+import { Input, TimePicker } from 'antd'
 import React from 'react'
 import './event_modal.css'
+import dayjs from 'dayjs'
+
 const modeOptions = ['create', 'edit'] as const
 type Props = {
 	mode: (typeof modeOptions)[number]
@@ -16,6 +18,14 @@ const EventModal = (props: Props) => {
 		<div>
 			<Input className='modalTitle' defaultValue={props.title} placeholder='Add Title' />
 			<ClockCircleOutlined />
+			<div className='time-ctn'>
+				<div className='date-str'>{props.timeStart.toLocaleString('en-US', { weekday: 'short' })}</div>
+				<div className='time-picker'>
+					<div>{props.timeStart.toLocaleTimeString('vi-VN')}</div>
+					<TimePicker defaultValue={dayjs(props.timeStart.toLocaleTimeString('vi-VN'), 'HH:mm')} format={'HH:mm'} />
+					<TimePicker defaultValue={dayjs(props.timeEnd.toLocaleTimeString('vi-VN'), 'HH:mm')} format={'HH:mm'} />
+				</div>
+			</div>
 			<Input placeholder='Add guests' />
 			<MenuOutlined />
 			<Input.TextArea rows={3} placeholder='Add description' defaultValue={props.description} />
