8f7e9f8129f6bd81e3c14365c0dd608b0dba7f31
Minh-19102 <minhdn.es76d@gmail.com>
2023-06-24 17:28:54 +0700

    update create event Modal

--- src/components/Modal/EventModal.tsx
+++ src/components/Modal/EventModal.tsx
@@ -1,8 +1,8 @@
-import { ClockCircleOutlined, MenuOutlined } from '@ant-design/icons'
-import { Input, TimePicker } from 'antd'
-import React from 'react'
+import { ClockCircleOutlined, MenuOutlined, TeamOutlined } from '@ant-design/icons'
+import { Divider, Input, TimePicker } from 'antd'
+import React, { useEffect, useState } from 'react'
 import './event_modal.css'
-import dayjs from 'dayjs'
+import dayjs, { Dayjs } from 'dayjs'
 
 const modeOptions = ['create', 'edit'] as const
 type Props = {
@@ -14,21 +14,53 @@ type Props = {
 }
 
 const EventModal = (props: Props) => {
+	const [timeStart, setTimeStart] = useState<Dayjs | null>(null)
+	const [timeEnd, setTimeEnd] = useState<Dayjs | null>(null)
+	const [title, setTitle] = useState('')
+	const [description, setDescription] = useState('')
+	useEffect(() => {
+		setTimeStart(dayjs(props.timeStart))
+		setTimeEnd(dayjs(props.timeEnd))
+		setTitle('')
+		setDescription('')
+	}, [props.timeStart, props.timeEnd])
+
+	const onChangeStart = (value: Dayjs | null) => {
+		setTimeStart(value)
+	}
+
+	const onChangeEnd = (value: Dayjs | null) => {
+		setTimeEnd(value)
+	}
 	return (
 		<div>
-			<Input className='modalTitle' defaultValue={props.title} placeholder='Add Title' />
-			<ClockCircleOutlined />
+			<Input className='modalTitle' placeholder='Add Title' value={title} onChange={(e) => setTitle(e.target.value)} />
+			<Divider className='title-divider' />
 			<div className='time-ctn'>
-				<div className='date-str'>{props.timeStart.toLocaleString('en-US', { weekday: 'short' })}</div>
+				<ClockCircleOutlined />
+				<div className='date-str'>{props.timeStart.toLocaleString('en-US', { weekday: 'short', day: 'numeric' })}</div>
 				<div className='time-picker'>
-					<div>{props.timeStart.toLocaleTimeString('vi-VN')}</div>
-					<TimePicker defaultValue={dayjs(props.timeStart.toLocaleTimeString('vi-VN'), 'HH:mm')} format={'HH:mm'} />
-					<TimePicker defaultValue={dayjs(props.timeEnd.toLocaleTimeString('vi-VN'), 'HH:mm')} format={'HH:mm'} />
+					<TimePicker className='tp' value={timeStart} onChange={onChangeStart} format={'HH:mm'} minuteStep={5} />
+					<div>ー</div>
+					<TimePicker className='tp' value={timeEnd} onChange={onChangeEnd} format={'HH:mm'} minuteStep={5} />
 				</div>
 			</div>
-			<Input placeholder='Add guests' />
-			<MenuOutlined />
-			<Input.TextArea rows={3} placeholder='Add description' defaultValue={props.description} />
+			<div className='team-ctn'>
+				<TeamOutlined />
+				<Input placeholder='Add guests' />
+			</div>
+			<div className='description-ctn'>
+				<MenuOutlined style={{ paddingTop: '5px' }} />
+				<Input.TextArea
+					rows={3}
+					placeholder='Add description'
+					defaultValue={props.description}
+					value={description}
+					onChange={(e) => {
+						setDescription(e.target.value)
+					}}
+				/>
+			</div>
 		</div>
 	)
 }
